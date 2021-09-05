4ced10507eac783ba20468158ccc665d79418aba
Jyoti Jangid <jyotik.cse17@nituk.ac.in>
2021-09-05 12:35:23 +0530

    code added

---
+++ src/test/scala/simulations/ProductInventorySimulation.scala
@@ -0,0 +1,46 @@
+package simulations
+
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+class ProductInventorySimulation extends Simulation {
+
+  //conf
+  val value_conf = http.baseUrl("http://localhost:8001")
+    .header("Accept",value="application/json")
+    .header(name="content-type", value ="application/json")
+
+
+  //scenario
+  val scn = scenario("Get all Product Inventory")
+    .exec(http("get all Product Inventory details")
+      .get("/rvy/api/pis/v1/products")
+      .check(status is 200)
+
+    )
+	 
+    .exec(http("Get Product Inventory By Id")
+      .get("/rvy/api/pis/v1/product/15")
+      .check(status is 200)
+    )
+	
+	.exec(http("Post Product Inventory")
+      .post("/rvy/api/pis/v1/product")
+      .body(RawFileBody(filePath = "./src/test/resources/bodies/addProductInventory.json")).asJson
+      .header(name="content-type",value = "application/json")
+      .check(status is 200))
+
+	.exec(http("Update Product Inventory")
+      .post("/rvy/api/pis/v1/product")
+      .body(RawFileBody(filePath = "./src/test/resources/bodies/updateProductInventory.json")).asJson
+      .header(name="content-type",value = "application/json")
+      .check(status is 200))
+
+    
+
+  //setup
+  setUp(scn.inject(atOnceUsers(users=100))).protocols(value_conf)
+
+
+
+}
